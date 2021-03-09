4a7d943a6b068a353041513c5098dfdb66cd0cd1
EC2 Default User <ec2-user@ip-172-31-80-205.ec2.internal>
2021-03-09 14:30:33 +0000

    [Finish2-8]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -14,7 +14,7 @@ class TodolistsController < ApplicationController
   #redirect_to'/top'を削除して、以下コードに変更
   #詳細画面にリダイレクト
   redirect_to todolist_path(list.id)
-  
+
   end
 
   def index
@@ -25,7 +25,7 @@ class TodolistsController < ApplicationController
   def show
     @list=List.find(params[:id])
   end
-  
+
   def edit
     @list=List.find(params[:id])
   end
@@ -33,7 +33,12 @@ class TodolistsController < ApplicationController
     list=List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
-  
+  end
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を一件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #投稿一覧画面へリダイレクト
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get'todolists/:id/edit'=>'todolists#edit',as:'edit_todolist'
   patch'todolists/:id'=>'todolists#update',as:'update
   _todolist'
+  delete'todolists/:id'=>'todolists#destroy',as:'destroy_todolist'
   
 end
