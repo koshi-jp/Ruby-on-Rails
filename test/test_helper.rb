a9e7da5dd41bc6d82ec7f4bcb2ab4e0d2f4ab4e9
EC2 Default User <koushi0324@gmail.com>
2021-03-29 06:44:00 +0000

    329

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,17 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  describe '四則演算' do
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+  end
   # Add more helper methods to be used by all tests here...
 end
